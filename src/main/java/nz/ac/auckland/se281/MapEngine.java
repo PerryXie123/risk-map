04ce8c8638270aa4f11141656b2219cd7f9fb547
Perry Xie <perryxie.nz@gmail.com>
2024-05-18 22:09:45 +1200

    Added message for when there is no cross border travel needed.

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,12 +1,20 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 /** This class is the main entry point. */
 public class MapEngine {
   List<Country> countryList = new LinkedList<>();
   boolean validCountry = false;
+  boolean validCountryStart = false;
+  boolean validCountryEnd = false;
+  MapGraph graph = new MapGraph();
+  int fee = 0;
 
   public MapEngine() {
     // add other code here if you want
@@ -22,7 +30,6 @@ public class MapEngine {
       Country countryInstance = new Country(countryInfo[0], countryInfo[1], countryInfo[2]);
       countryList.add(countryInstance);
     }
-    MapGraph graph = new MapGraph();
     graph.loadCountries(adjacencies, countryList);
   }
 
@@ -41,7 +48,7 @@ public class MapEngine {
         country = Utils.capitalizeFirstLetterOfEachWord(country);
       }
     }
-
+    validCountry = false;
     for (Country countryComp : countryList) {
       if (country.equals(countryComp.getCountry())) {
         MessageCli.COUNTRY_INFO.printMessage(
@@ -51,7 +58,63 @@ public class MapEngine {
   }
 
   /** this method is invoked when the user run the command route. */
-  public void showRoute() {}
+  public void showRoute() {
+    List<Country> path = new ArrayList<>();
+
+    String countryStart;
+    String countryEnd;
+
+    Country startCountry = null;
+    Country endCountry = null;
+
+    MessageCli.INSERT_SOURCE.printMessage();
+    countryStart = Utils.scanner.nextLine();
+    countryStart = Utils.capitalizeFirstLetterOfEachWord(countryStart);
+
+    while (validCountry == false) {
+      try {
+        findCountry(countryStart);
+      } catch (InvalidCountryException e) {
+        MessageCli.INVALID_COUNTRY.printMessage(countryStart);
+        countryStart = Utils.scanner.nextLine();
+        countryStart = Utils.capitalizeFirstLetterOfEachWord(countryStart);
+      }
+    }
+
+    validCountry = false;
+
+    MessageCli.INSERT_DESTINATION.printMessage();
+    countryEnd = Utils.scanner.nextLine();
+    countryEnd = Utils.capitalizeFirstLetterOfEachWord(countryEnd);
+
+    while (validCountry == false) {
+      try {
+        findCountry(countryEnd);
+      } catch (InvalidCountryException e) {
+        MessageCli.INVALID_COUNTRY.printMessage(countryEnd);
+        countryEnd = Utils.scanner.nextLine();
+        countryEnd = Utils.capitalizeFirstLetterOfEachWord(countryEnd);
+      }
+    }
+
+    for (Country country : countryList) {
+      if (countryStart.equals(country.getCountry())) {
+        startCountry = country;
+      }
+      if (countryEnd.equals(country.getCountry())) {
+        endCountry = country;
+      }
+    }
+
+    if(countryStart.equals(countryEnd)){
+      MessageCli.NO_CROSSBORDER_TRAVEL.printMessage();
+      return;
+    }
+
+    path = breadthFirstTraversal(startCountry, endCountry);
+
+
+  }
 
   public void findCountry(String countryInput) {
     int count = 0;
@@ -66,5 +129,37 @@ public class MapEngine {
     } else {
       validCountry = true;
     }
+  }
+
+  public List<Country> breadthFirstTraversal(Country root, Country target) {
+    List<Country> visited = new ArrayList<>();
+    Queue<Country> queue = new LinkedList<>();
+    Map<Country, Country> predecessors = new HashMap<>();
+
+    queue.add(root);
+    visited.add(root);
+    predecessors.put(root, null);
+
+    while (!queue.isEmpty()) {
+      Country current = queue.poll();
+
+      if (current.equals(target)) {
+        List<Country> path = new LinkedList<>();
+        for (Country at = target; at != null; at = predecessors.get(at)) {
+          path.add(0, at);
+        }
+        return path;
+      }
+
+      for (Country neighbor : graph.adjCountries.get(current)) {
+        if (!visited.contains(neighbor)) {
+          queue.add(neighbor);
+          visited.add(neighbor);
+          predecessors.put(neighbor, current);
+        }
+      }
+    }
+
+    return new ArrayList<>(); // Return an empty list if no path found
   }
 }
