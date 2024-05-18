b1b301285bdfd2f5302f428b479e66bb34babb84
Perry Xie <perryxie.nz@gmail.com>
2024-05-18 21:02:31 +1200

    Created a custom exception to handle invalid country names.

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -6,6 +6,7 @@ import java.util.List;
 /** This class is the main entry point. */
 public class MapEngine {
   List<Country> countryList = new LinkedList<>();
+  boolean validCountry = false;
 
   public MapEngine() {
     // add other code here if you want
@@ -28,13 +29,41 @@ public class MapEngine {
   /** this method is invoked when the user run the command info-country. */
   public void showInfoCountry() {
     String country = Utils.scanner.nextLine();
+    country = Utils.capitalizeFirstLetterOfEachWord(country);
+
+    while (validCountry == false) {
+      try {
+        findCountry(country);
+      } catch (InvalidCountryException e) {
+        MessageCli.INVALID_COUNTRY.printMessage(country);
+        country = Utils.scanner.nextLine();
+        country = Utils.capitalizeFirstLetterOfEachWord(country);
+      }
+    }
+
     for (Country countryComp : countryList) {
-      if(country.equals(countryComp.getCountry())){
-        MessageCli.COUNTRY_INFO.printMessage(countryComp.getCountry(), countryComp.getContinent(), countryComp.getTax());
+      if (country.equals(countryComp.getCountry())) {
+        MessageCli.COUNTRY_INFO.printMessage(
+            countryComp.getCountry(), countryComp.getContinent(), countryComp.getTax());
       }
     }
   }
 
   /** this method is invoked when the user run the command route. */
   public void showRoute() {}
+
+  public void findCountry(String countryInput) {
+    int count = 0;
+    for (Country country : countryList) {
+      if (countryInput.equals(country.getCountry())) {
+        count++;
+      }
+    }
+    if (count == 0) {
+      validCountry = false;
+      throw new InvalidCountryException();
+    } else {
+      validCountry = true;
+    }
+  }
 }
